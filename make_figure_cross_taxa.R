8d0ef55bba7f1f7a65cc2ed175eedd5b69f46ab5
Aurore Maureaud <aurore.maureaud@gmail.com>
2021-05-06 10:13:25 -0400

    orga of the repo

---
+++ make_figure_cross_taxa.R
@@ -0,0 +1,17 @@
+###############################################################################
+#### Figures showing the taxonomic descriptions through time across taxa
+#### Coding and data processing: Aurore Maureaud
+#### May 2021
+################################################################################
+
+rm(list = ls())
+
+# set date
+date <- '6MAY2021'
+
+# libraries
+library(ggplot2)
+library(tidyverse)
+
+
+### 1. Load data
