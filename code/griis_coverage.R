dd86a5b215a41ad563113ed450268baa86f682c1
Aurore Maureaud <aurore.maureaud@gmail.com>
2021-11-11 10:44:03 -0500

    create griis code

---
+++ code/griis_coverage.R
@@ -0,0 +1,24 @@
+################################################################################
+#### Assessment of IUCN coverage per group
+#### Coding and data processing: Maisha Lucas
+#### November 2021
+################################################################################
+
+rm(list = ls())
+
+# set date
+date <- '11NOV2021'
+
+# libraries
+library(ggplot2)
+library(tidyverse)
+library(readxl)
+library(rphylopic)
+library(egg)
+library(grid)
+library(png)
+library(writexl)
+library(rredlist)
+
+# load data
+taxonomies <- read.csv("data/taxonomies_11NOV2021.csv")
