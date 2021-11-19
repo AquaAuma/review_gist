c033bb5a07572b50f832032257562f378f20622c
Aurore Maureaud <aurore.maureaud@gmail.com>
2021-11-19 11:42:46 -0500

    started getting phylo tree data for dragonflies

--- code/tree_coverage.R
+++ code/tree_coverage.R
@@ -22,4 +22,30 @@ library(rredlist)
 
 # load data
 taxonomies <- read.csv("data/taxonomies_11NOV2021.csv")
+phylotree <- read_csv(file = "C:/Users/auror/Downloads/OpenTreePhylogeny_assessment/OpenZoom_PhylogeneticTaxonomy1121.csv")
 
+
+################################################################################
+#### 1. MATCH FUNCTION
+################################################################################
+match_tree <- function(taxonomies, ncbi_raw, group) {
+  
+}
+
+
+################################################################################
+#### 2. APPLY TO GROUPS
+################################################################################
+
+### A. dragonflies #############################################################
+
+uid_position <- which(phylotree$name == "Odonata")
+uid_dragonflies <- phylotree$uid[uid_position]
+tree_dragonflies <- phylotree[uid_position,]
+
+while(rank!="species"){
+  uid_positions <- which(phylotree$parent_uid == uid_dragonflies)
+  uid_children <- phylotree$uid[uid_positions]
+  tree_dragonflies <- rbind(tree_dragonflies, phylotree[uid_positions,])
+  
+}
